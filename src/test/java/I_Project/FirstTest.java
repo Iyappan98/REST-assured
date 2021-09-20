3e7bdec048d1c600383f94d229ca3e9450e9d5d2
Iyappan <I3091@IND-CHN-LT10722.indium.com>
2021-09-20 23:37:56 +0530

    created the branch and add console msg

--- src/test/java/I_Project/FirstTest.java
+++ src/test/java/I_Project/FirstTest.java
@@ -11,6 +11,7 @@ public class FirstTest {
 		String URI = "https://heisenbergproxies2.azurewebsites.net/int/auth/user/authenticate";
 		String token = restAssured.postCall(URI, 200, "authToken");	
 		restAssured.getToken(token);
+		System.out.println("Auth token printed successfully");
 	}
 
 }
