d79317da660ff3be60f4278573d0f5c35d0b8232
Iyappan <I3091@IND-CHN-LT10722.indium.com>
2021-09-22 22:59:20 +0530

    test data added

--- src/test/java/I_Project/FirstTest.java
+++ src/test/java/I_Project/FirstTest.java
@@ -1,15 +1,18 @@
 package I_Project;
 
+import java.io.IOException;
 import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 	
 	@Test
-	public void getAuthToken() throws InterruptedException, ParseException {
+	public void getAuthToken() throws InterruptedException, ParseException, IOException {
 		RA_Utilities restAssured = new RA_Utilities();
-		String URI = "https://heisenbergproxies2.azurewebsites.net/int/auth/user/authenticate";
-		String token = restAssured.postCall(URI, 200, "authToken");	
+		String uri = restAssured.retrive("uri");
+		String payload = restAssured.retrive("payload");
+		String statusCode = restAssured.retrive("statusCode");
+		String token = restAssured.postCall(uri, statusCode, payload);	
 		restAssured.getToken(token);
 		System.out.println("Auth token printed successfully");
 	}
--- src/test/java/I_Project/RA_Utilities.java
+++ src/test/java/I_Project/RA_Utilities.java
@@ -3,109 +3,77 @@ package I_Project;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.*;
-
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
-
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class RA_Utilities {
+
+	public String getCall(String URI, int statusCode) {
+		Response response = given().get(URI).then().statusCode(statusCode).extract().response();
+		String responseBody = response.asString();
+		return responseBody;
+	}
+
+	public String postCall(String URI, String statusCode, String payloadName) {
+		File Payload = new File("src/test/resources/" + payloadName + ".json");
+		Response response = given().headers("Content-type", "application/json").body(Payload).when().post(URI).then()
+				.statusCode(Integer.parseInt(statusCode)).extract().response();
+		String responseBody = response.asString();
+		System.out.println(responseBody);
+		return responseBody;
+	}
+
+	@SuppressWarnings("unchecked")
+	public String putCall(String URI, int statusCode, String payloadName) throws ParseException {
+		File Payload = new File("src/test/resources/" + payloadName + ".json");
+		JSONParser parser = new JSONParser();
+		String payload = (String) Payload.toString();
+		JSONObject updatedPayload = (JSONObject) parser.parse(payload);
+		updatedPayload.put("name", "Iyappan");
+		Response response = given().body(updatedPayload).when().put(URI).then().statusCode(200).extract().response();
+		String responseBody = response.asString();
+		return responseBody;
+	}
+
+	public void patchCall() {
+		JSONObject request = new JSONObject();
+
+		// another method
+		request.put("name", "Iyappan");
+		request.put("job", "Tester");
+
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+		System.out.println(request.toString());
+
+		baseURI = "https://reqres.in";
+
+		given().body(request.toJSONString()).when().patch("/api/users/2").then().statusCode(204).log().all();
+	}
+
+	public void deleteCall(String URI) {
+		given().when().patch(URI).then().statusCode(204);
+	}
+
+	public String getToken(String response) throws ParseException {
+		JSONParser parser = new JSONParser();
+		JSONObject a = (JSONObject) parser.parse(response);
+		String responseBody = a.get("jwt").toString();
+		System.out.println(responseBody);
+		return responseBody;
+	}
 	
-	public String getCall(String URI, int statusCode) {			
-			Response response =given().
-					             get(URI).
-			                   then().
-			                     statusCode(statusCode).
-			                   extract().
-			                     response();
-			String responseBody = response.asString();
-			return responseBody;
-		}
-		
-	public String postCall(String URI,int statusCode, String payloadName) {
-	    	File Payload = new File("src/test/resources/"+payloadName+".json");			
-			Response response = given().
-			                      headers("Content-type","application/json").
-			                      body(Payload).
-			                    when().
-			                      post(URI).
-			                    then().
-			                      statusCode(statusCode).
-			                      extract().
-                                  response();
-            String responseBody = response.asString();
-            System.out.println(responseBody);
-            return responseBody;
-		}
-		
-		//@Test
-		@SuppressWarnings("unchecked")
-		public String putCall(String URI,int statusCode, String payloadName) throws ParseException {
-			File Payload = new File("src/test/resources/"+payloadName+".json");	
-			JSONParser parser = new JSONParser();
-	        String payload = (String) Payload.toString();
-	        JSONObject updatedPayload = (JSONObject) parser.parse(payload);
-	        updatedPayload.put("name", "Iyappan");			
-	        Response response = given().
-			                      body(updatedPayload).
-			                    when().
-			                      put(URI).
-			                      then().
-			                      statusCode(200).
-			                      extract().
-                                  response();
-	        String responseBody = response.asString();
-            return responseBody;
-		}
-		
-		//@Test
-		public void patchCall() {
-	       JSONObject request = new JSONObject();
-			
-			//another method
-			request.put("name", "Iyappan");
-			request.put("job", "Tester");
-			
-			
-			System.out.println(request);
-			System.out.println(request.toJSONString());
-			System.out.println(request.toString());
-			
-			baseURI = "https://reqres.in";
-			
-			given().
-			  body(request.toJSONString()).
-			when().
-			  patch("/api/users/2").
-			then().
-			  statusCode(204).
-			  log().all();
-		}
-		
-			public void deleteCall(String URI) {			
-			given().
-			when().
-			  patch(URI).
-			then().
-			  statusCode(204);
-		}
-			
-			public String getToken(String response) throws ParseException {
-				JSONParser parser = new JSONParser();
-				JSONObject a = (JSONObject) parser.parse(response);
-				String responseBody = a.get("jwt").toString();
-				System.out.println(responseBody);
-				return responseBody;
-			}
+	public String retrive(String Value) throws IOException, ParseException {
+		FileReader file = new FileReader("src/test/resources/Testdata.json");
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(file);
+		JSONObject data = (JSONObject) obj;
+		String testData = data.get(Value).toString();
+		return testData;
+	}
 
 }
